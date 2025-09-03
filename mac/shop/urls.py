4ab8970f4d2ea39b758e62b28a53e96e2c1f9dfa
arpitkatiyar108 <arpitkatiyar108@gmail.com>
2025-09-03 22:46:13 +0530

    Complete-video-23: E-commerce Website: Project Layout

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -1,8 +1,13 @@
+# shop/urls.py
 from django.urls import path
-from . import views
+from . import views  # Import views from this app
 
-app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='home'),
-    # other blog routes
+    path('', views.home, name='shop_home'),
+    path('about/', views.about, name='shop_about'),
+    path('contact/', views.contact, name='shop_contact'),
+    path('tracker/', views.order_tracker, name='shop_tracker'),
+    path('search/', views.search_products, name='shop_search'),
+    path('product/<int:id>/', views.product_detail, name='shop_product_detail'),
+    path('checkout/', views.checkout, name='shop_checkout'),
 ]
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
+# Homepage
 def home(request):
-    return render(request, 'shop/home.html')
+    return HttpResponse("Welcome to My Awesome Cart - Home Page")
+
+# About Us Page
+def about(request):
+    return HttpResponse("About Us - My Awesome Cart")
+
+# Contact Page
+def contact(request):
+    return HttpResponse("Contact Us - My Awesome Cart")
+
+# Order Tracker Page
+def order_tracker(request):
+    return HttpResponse("Track Your Order")
+
+# Search Products Page
+def search_products(request):
+    return HttpResponse("Search Products Page")
+
+# Product Detail Page
+def product_detail(request, id):
+    return HttpResponse(f"Product Detail Page for Product ID: {id}")
+
+# Checkout Page
+def checkout(request):
+    return HttpResponse("Checkout Page")
