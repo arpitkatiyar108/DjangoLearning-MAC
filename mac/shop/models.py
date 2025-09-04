88f4175020e6e2014a03ba6fd143b4c94dec9f58
arpitkatiyar108 <arpitkatiyar108@gmail.com>
2025-09-04 20:25:58 +0530

    Complete-video-27: Add Images & Change Models In Django

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -5,7 +5,11 @@ class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=50)
     desription = models.CharField(max_length=300)
+    category = models.CharField(max_length=50, default='')
+    subcategory = models.CharField(max_length=50, default='')
+    price = models.FloatField(default=0)
     pub_date = models.DateField()
+    image = models.ImageField(upload_to='shop/images', default="")
 
     def __str__(self):
         return self.product_name
