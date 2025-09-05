7029ddacf2977e64a7831e18c710fa5b8dfab860
arpitkatiyar108 <arpitkatiyar108@gmail.com>
2025-09-05 22:36:13 +0530

    Complete-video-33: Show Products On The Homepage

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
+from math import ceil
 
 # Homepage
 def home(request):
-    return render(request, 'shop\home.html')
+    products = Product.objects.all()
+    n = len(products)
+    nSlides = n // 4 + ceil((n / 4) - (n // 4))
+    params = {
+        'products' : products,
+        'no_of_slides' : nSlides,
+        'range' : range(1, nSlides+1),
+        'no_of_purducts': n,
+        'no_of_products_per_slide' :  range(1, 4),
+    }
+    return render(request, 'shop\home.html', params)
 
 # About Us Page
 def about(request):
